84c38a752b55fda2bb5d419320fd8324eec2e708
Supun Madhusanka <tasmadhusanka@students.nsbm.ac.lk>
2026-01-30 10:32:41 +0530

    Implement user input functionality and move script to external file

---
+++ script.js
@@ -0,0 +1,6 @@
+    function getmsg(){
+        var name=document.getElementById("textname").value;
+        var name2=document.getElementById("textname2").value;
+
+        alert("hello "+name+" "+name2);
+     }
