709781500b2f96c2aea302ca6661dd3a1c231a46
Supun Madhusanka <tasmadhusanka@students.nsbm.ac.lk>
2026-01-31 12:25:27 +0530

    array

---
+++ script3.js
@@ -0,0 +1,34 @@
+/* let hobbies=[];
+let elements=hobbies.push("coding","cricket","cooking");
+console.log(elements) */
+
+
+
+//push element to array and access element
+
+/* let hobbies=[];
+hobbies.push("coding","cricket","cooking");
+
+console.log(hobbies[1]); */
+
+
+
+
+//remove element from array
+/* let hobbies=[];
+hobbies.push("coding","cricket","cooking");
+
+console.log(hobbies)
+let element=hobbies.pop();
+console.log(hobbies)
+console.log(element); */
+
+
+
+ //accese to elements in array
+let hobbies=[];
+hobbies.push("coding","cricket","cooking");
+
+hobbies.forEach(hobby=>{
+    console.log(hobby);
+}) 
