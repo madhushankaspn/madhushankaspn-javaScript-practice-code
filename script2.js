2406001d4c5b07e5fb5e1faf510c2cba0e22f353
Supun Madhusanka <tasmadhusanka@students.nsbm.ac.lk>
2026-01-31 10:52:52 +0530

    var,let and const

--- script2.js
+++ script2.js
@@ -1,11 +1,31 @@
-let color ="red";
+/* let color ="red";
 color="blue";
 
 function getColor(){
     let color="green";
     color="yellow";
     console.log(color);
-
+ 
 }
 getColor();
-console.log(color);
+console.log(color); */
+ 
+
+
+
+// Const varible value cannot be changed,but for object it can be changed
+/*  const username={
+    name:"John",
+ }
+ 
+ username.name="supun"
+
+ console.log(username.name); */
+
+
+
+//also const value can be change using array methods
+ const colors=["red","blue"];
+ colors.push("green");
+ console.log(colors);
+
