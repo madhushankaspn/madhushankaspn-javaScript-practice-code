076fa663d356bdbc023eac975cd5eda7d322c521
Supun Madhusanka <tasmadhusanka@students.nsbm.ac.lk>
2026-02-02 00:08:20 +0530

    Refactor employee object to include a method for message retrieval and update last name

--- script4.js
+++ script4.js
@@ -20,11 +20,23 @@ console.log(employee); */
 
 
 //Edit value in object
-let employee={
+/* let employee={
     firstName:"Supun",
     LastName:"Madhushanka",
     age:24
 }
 employee.country="Sri Lanka";
 employee.LastName="Perera";
-console.log(employee);
+console.log(employee); */
+
+
+
+let employee={
+    firstName:"Supun",
+    LastName:"Madhushanka",
+    age:24,
+    getmsg:function(s1){
+        alert(s1+" " +this.LastName);
+    }
+}
+console.log(employee.getmsg("Supun"));
