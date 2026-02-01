8a828edfd58ab273bf198a10bd60b3fe99661fd5
Supun Madhusanka <tasmadhusanka@students.nsbm.ac.lk>
2026-02-01 20:21:54 +0530

    Add HTML structure and JavaScript functionality for employee object manipulation

---
+++ script4.js
@@ -0,0 +1,30 @@
+/* let employee={
+    firstName:"Supun",
+    LastName:"Madhushanka",
+    age:24
+}
+console.log(employee);
+console.log(employee.firstName);
+console.log(employee.LastName);
+console.log(employee.age); */
+
+
+//Add avalue to object
+/* let employee={
+    firstName:"Supun",
+    LastName:"Madhushanka",
+    age:24
+}
+employee.country="Sri Lanka";
+console.log(employee); */
+
+
+//Edit value in object
+let employee={
+    firstName:"Supun",
+    LastName:"Madhushanka",
+    age:24
+}
+employee.country="Sri Lanka";
+employee.LastName="Perera";
+console.log(employee);
