38596a31c27ad80673e5e01e8ae47e8b5853e4d8
Supun Madhusanka <tasmadhusanka@students.nsbm.ac.lk>
2026-02-04 22:52:14 +0530

    || (OR) operator

--- script6.js
+++ script6.js
@@ -1,4 +1,4 @@
-let a=2;
+/* let a=2;
 let b=3;
 let c=4;
 
@@ -8,5 +8,26 @@ if(a==2 && b==3){
     console.log("condition 1 is true");
 }else{
     
-    console.log("condition 2 is true");
+    console.log("all are false");
+} */
+
+
+
+
+
+//|| (OR) operator
+
+let a=2;
+let b=3;
+let c=4;
+
+
+
+if(a==4 || b==3){
+    console.log("condition 1 is true");
+}else{
+    
+    console.log("all are false");
 }
+
+
