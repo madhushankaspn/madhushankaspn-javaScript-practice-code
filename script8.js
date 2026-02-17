bd04f40c6ba113cefb4c64a2eab6cd5645ed3112
Supun Madhusanka <tasmadhusanka@students.nsbm.ac.lk>
2026-02-17 21:48:22 +0530

    assignment operator examples

---
+++ script8.js
@@ -0,0 +1,16 @@
+//Assignment operator
+
+let a = 10;
+a = a + 5; // a is now 15
+
+console.log(a); // Output: 15
+
+//Using shorthand assignment operator
+a += 5; // a is now 20
+console.log(a); // Output: 20
+
+
+
+// -= operator
+a -= 3; // a is now 17   (a = a - 3)
+console.log(a); // Output: 17
