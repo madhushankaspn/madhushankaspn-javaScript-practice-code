f68dd2a007d60ed3fcbaeb6dedabd5c03afe2798
Supun Madhusanka <tasmadhusanka@students.nsbm.ac.lk>
2026-02-17 22:09:54 +0530

    add form elements and calculation functionality in prac8.html and script8.js

--- script8.js
+++ script8.js
@@ -1,4 +1,4 @@
-//Assignment operator
+/* //Assignment operator
 
 let a = 10;
 a = a + 5; // a is now 15
@@ -13,4 +13,21 @@ console.log(a); // Output: 20
 
 // -= operator
 a -= 3; // a is now 17   (a = a - 3)
-console.log(a); // Output: 17
+console.log(a); // Output: 17 */
+
+
+function calculateTotal(){
+    let price = document.getElementById("price").value;
+    let quantity = document.getElementById("quantity").value;
+
+    let totalPrice = price*quantity;
+    document.getElementById("totalPrice").value = totalPrice;
+
+}
+
+let calculate = document.getElementById("calculate");
+
+calculate.addEventListener("click",calculateTotal);
+
+
+ 
