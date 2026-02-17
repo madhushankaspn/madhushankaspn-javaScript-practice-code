aab71a52e85a34c1cede5a76ab360c4e6783f26f
Supun Madhusanka <tasmadhusanka@students.nsbm.ac.lk>
2026-02-17 17:59:45 +0530

    arithmetic operations examples in script7.js

--- script7.js
+++ script7.js
@@ -1,6 +1,36 @@
-let a = 20;
+/* let a = 20;
 let b = 10;
 let c = "Hello";
 
 console.log("a + b + c =", a + b + c);
-console.log("a + b =", a + b);
+console.log("a + b =", a + b); */
+
+
+/* 
+let a = 20;
+let b = 10;
+let c = "Hello";
+
+console.log("a - b + c =", a - b + c);
+console.log("a - b =", a - b); */
+
+
+
+/* let a = 20;
+let b = 10;
+let c = "Hello";
+
+console.log("a * b + c =", a * b + c);
+console.log("a * b =", a * b); */
+
+
+
+
+/* let a = 20;
+let b = 10;
+let c = "Hello";
+
+console.log("a / b + c =", a / b + c);
+console.log("a / b =", a / b); */
+
+
