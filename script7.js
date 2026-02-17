8ba183a46b1559057aa96f285bf0194ab3db9652
Supun Madhusanka <tasmadhusanka@students.nsbm.ac.lk>
2026-02-17 20:36:25 +0530

    increment operators in script7.js

--- script7.js
+++ script7.js
@@ -34,3 +34,42 @@ console.log("a / b + c =", a / b + c);
 console.log("a / b =", a / b); */
 
 
+
+//Modulus operator
+
+/*  let a = 20;
+let b = 8;
+let c = "Hello";
+
+console.log("a % b + c =", a % b + c);
+console.log("a % b =", a % b);  */
+
+
+
+
+//Exponentiation operator
+
+/* let a = 2;
+let b = 3;
+
+console.log("a ** b =", a ** b);  */
+
+
+//Increment operator
+//post-increment
+/* let a = 5;
+let b =2;
+
+let c = a++;
+console.log("Value of c after post-increment:", c);
+console.log("Value of a after post-increment:", a); */// Outputs 5
+
+//pre-increment
+let a = 5;
+let b =2;
+
+let c = ++a;
+console.log("Value of c after pre-increment:", c);
+console.log("Value of a after pre-increment:", a);
+
+
