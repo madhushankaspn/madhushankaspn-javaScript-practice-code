b7fdc85189e2d57138894be948dc7d0bcae56dad
Supun Madhusanka <tasmadhusanka@students.nsbm.ac.lk>
2026-02-23 07:38:45 +0530

    comparison operators

--- script9.js
+++ script9.js
@@ -10,7 +10,54 @@ console.log(a == b); // true */
 
 
 // === strict equal to  
-let a = 5;
+/* let a = 5;
 let b = "5";
 console.log(a === b);
+ */
 
+
+
+
+// != not equal to
+
+/* let a = 5;
+let b = 10;
+console.log(a != b);  */// true
+ 
+
+// !== strict not equal to
+
+/* let a = 5;
+let b = "5";
+console.log(a !== b);  */// false  
+
+
+// > grater than
+/* 
+let x =5;
+let y = 10;
+console.log(x > y); // false
+
+// < less than
+
+let x =5;
+let y = 10;
+console.log(x < y); // true
+
+// >= greater than or equal to
+
+let x =5;
+let y = 10;
+console.log(x >= y); // false
+
+// <= less than or equal to
+
+let x =5;
+let y = 10;
+console.log(x <= y); // true
+ */
+
+let x ="50";
+let y = "55";
+
+console.log(Number(x) > Number(y));
