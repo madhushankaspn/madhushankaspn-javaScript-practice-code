1fe9aac36232d0a8d35be001237e364a026050e8
Supun Madhusanka <tasmadhusanka@students.nsbm.ac.lk>
2026-03-02 11:51:24 +0530

    for loop

---
+++ script12.js
@@ -0,0 +1,23 @@
+//for loop example
+/* for(let i=0; i<=20; i+=2){
+
+    if(i===0){
+
+    continue;
+    }
+
+
+    if(i===14){
+        break;
+    }
+   console.log(i);
+}
+  */
+
+
+
+
+
+//while loop example
+
+
