aa3906364ae321899fed4ec049d5a7f5e52b4888
Supun Madhusanka <tasmadhusanka@students.nsbm.ac.lk>
2026-03-03 22:12:22 +0530

    refactor event handlers and listeners for clarity

--- script13.js
+++ script13.js
@@ -3,11 +3,46 @@
 const btn = document.getElementById("btn");
 
 btn.onclick = function(){
-    console.log("first click");
+   // console.log("first click");
 }
 
 btn.onclick = function(){
-    console.log("second click");
-}
+    //console.log("second click");
+} 
 
 //only the second click will be executed because the first one is overwritten by the second one.
+
+
+
+
+
+
+
+//event listeners
+
+btn.addEventListener("click",function(){
+    //console.log("first click");
+
+})
+
+btn.addEventListener("click",function(){
+    //console.log("second click");
+})
+
+btn.addEventListener("click",function(){
+    //console.log("third click");
+})
+
+
+
+window.addEventListener("offline",function(){
+    alert("sorry we are offline")
+});
+
+window.addEventListener("online",function(){
+    alert("we are back online") 
+});
+
+
+
+
