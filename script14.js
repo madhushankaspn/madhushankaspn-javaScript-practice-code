cdec4638e211423a49a0e219d9317b069e07f613
Supun Madhusanka <tasmadhusanka@students.nsbm.ac.lk>
2026-04-09 11:46:59 +0530

    add styling for sections and images using getElementsByClassName and getElementsByTagName

--- script14.js
+++ script14.js
@@ -12,3 +12,26 @@ heading.style.fontFamily = "Arial";
 
 //====getElementsByClassName()====
 
+const section = document.getElementsByClassName("section");
+for (let i=0;i<section.length;i++){
+
+    section[i].style.color = "red";
+    section[i].style.fontFamily = "Verdana";
+    section[i].style.fontSize = "18px";
+
+} 
+
+//====getElementsByTagName()====
+
+const imagarea = document.getElementById("imagarea");
+const img = imagarea.getElementsByTagName("img");
+for(let i=0; i<img.length;i++){
+    img[i].style.width = "100%";
+    img[i].style.maxHeight = "290px";
+    img[i].style.objectFit = "cover";
+
+}
+
+
+//====querySelector()====
+
