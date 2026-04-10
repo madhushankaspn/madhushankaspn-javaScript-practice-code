daf6527537e8a078c2e7ecd551fe2198b2f79a22
Supun Madhusanka <tasmadhusanka@students.nsbm.ac.lk>
2026-04-10 13:31:39 +0530

    refactor formatting and spacing in script14.js for improved readability

--- script14.js
+++ script14.js
@@ -9,27 +9,24 @@ heading.style.color = "blue";
 heading.style.fontFamily = "Arial";
 
 
-
 //====getElementsByClassName()====
 
 const section = document.getElementsByClassName("section");
-for (let i=0;i<section.length;i++){
-
+for (let i = 0; i < section.length; i++) {
     section[i].style.color = "red";
     section[i].style.fontFamily = "Verdana";
     section[i].style.fontSize = "18px";
+}
 
-} 
 
 //====getElementsByTagName()====
 
 const imagarea = document.getElementById("imagarea");
 const img = imagarea.getElementsByTagName("img");
-for(let i=0; i<img.length;i++){
+for (let i = 0; i < img.length; i++) {
     img[i].style.width = "100%";
     img[i].style.maxHeight = "290px";
     img[i].style.objectFit = "cover";
-
 }
 
 
@@ -39,17 +36,16 @@ const bodycontent = document.querySelector("body");
 bodycontent.style.backgroundColor = "#88ffc2";
 
 const items = document.querySelector(".items");
-items.innerHTML += "<li>New Item 1</li> <li>New Item 2</li>";
+items.innerHTML += "<li>New Item 1</li><li>New Item 2</li>";
 
-
-const galleryimages = document.querySelector(".galleryimages");
-galleryimages.style.display = "flex";
-galleryimages.style.flexWrap = "wrap";
+const galleryContainer = document.querySelector(".galleryimages");
+galleryContainer.style.display = "flex";
+galleryContainer.style.flexWrap = "wrap";
 
 
 //====querySelectorAll()====
 
-const galleryimages = document.querySelectorAll(".galleryimages");
-galleryimages.forEach((e)=> {
+const galleryImages = document.querySelectorAll(".galleryimages img");
+galleryImages.forEach((e) => {
     e.style.maxWidth = "50%";
-})
+});
