6c8d06ab0f5c0b60b68fd9df6c64d699913e2933
Supun Madhusanka <tasmadhusanka@students.nsbm.ac.lk>
2026-04-14 11:51:37 +0530

    fix typo in setInterval comment and implement timer functionality

--- script15.js
+++ script15.js
@@ -1,2 +1,11 @@
-//setinterval()
+//setInterval()
+
+const timer = document.getElementById("timer");
+
+function getTime(){
+    let a = new Date().toLocaleTimeString();
+    timer.textContent = a;
+}
+
+setInterval(getTime, 1000);
 
